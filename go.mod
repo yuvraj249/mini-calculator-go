a70e394c34990b3587d3c0205fc36eb50666d716
yuvraj249 <yuvraj@tech-x.com>
2025-11-18 16:26:52 +0530

    commit for addition

---
+++ go.mod
@@ -0,0 +1,3 @@
+module mini-calculator-go
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func ADD(a, b int) int {
+	return a + b
+
+}
+
+func main() {
+	result := ADD(5, 6)
+	fmt.Println(result)
+
+}
