3048bee418a0f215b967c36a68900fb4ac9f1503
yuvraj249 <yuvraj@tech-x.com>
2025-11-18 16:32:09 +0530

    commit for division

--- main.go
+++ main.go
@@ -15,10 +15,15 @@ func MUL(a, b int) int {
 	return a * b
 }
 
+func DIV(a, b int) int {
+	return a * b
+}
+
 func main() {
 	result := ADD(5, 6)
 	fmt.Println(result)
 	fmt.Println(SUB(5, 6))
 	fmt.Println(MUL(5, 6))
+	fmt.Println(DIV(5, 6))
 
 }
