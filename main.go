0dd7204b84769fbf0865318fe905f6a1d4ede78f
yuvraj249 <yuvraj@tech-x.com>
2025-11-18 16:39:34 +0530

    commit for cli subtraction

--- main.go
+++ main.go
@@ -10,24 +10,15 @@ func CliADD(a, b int) int {
 
 }
 
-func SUB(a, b int) int {
+func CliSUB(a, b int) int {
 	return a - b
-}
 
-func MUL(a, b int) int {
-	return a * b
-}
-
-func DIV(a, b int) int {
-	if b == 0 {
-		return 0
-	}
-	return a / b
 }
 
 func main() {
 	a := flag.Int("a", 0, "number 1")
 	b := flag.Int("b", 0, "number 2")
 	fmt.Println(CliADD(*a, *b))
+	fmt.Println(CliSUB(*a, *b))
 
 }
