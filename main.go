56d0d303d2267127585c950936b3c18b728b16bf
yuvraj249 <yuvraj@tech-x.com>
2025-11-18 16:41:28 +0530

    commit for cli division

--- main.go
+++ main.go
@@ -20,11 +20,20 @@ func CliMUL(a, b int) int {
 
 }
 
+func CliDIV(a, b int) int {
+	if b == 0 {
+		return 0
+	}
+	return a / b
+
+}
+
 func main() {
 	a := flag.Int("a", 0, "number 1")
 	b := flag.Int("b", 0, "number 2")
 	fmt.Println(CliADD(*a, *b))
 	fmt.Println(CliSUB(*a, *b))
 	fmt.Println(CliMUL(*a, *b))
+	fmt.Println(CliDIV(*a, *b))
 
 }
