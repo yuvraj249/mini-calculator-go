3903c6a1bb5aee9230d66a22c3014882fa1e61ab
yuvraj249 <yuvraj@tech-x.com>
2025-11-18 16:31:20 +0530

    commit for multiplication

--- main.go
+++ main.go
@@ -11,9 +11,14 @@ func SUB(a, b int) int {
 	return a - b
 }
 
+func MUL(a, b int) int {
+	return a * b
+}
+
 func main() {
 	result := ADD(5, 6)
 	fmt.Println(result)
 	fmt.Println(SUB(5, 6))
+	fmt.Println(MUL(5, 6))
 
 }
